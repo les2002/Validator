7525d2b8a67b5a12975ab25bdf0613df5a82748f
les2002 <les2002@protonmail.com>
2017-10-27 16:16:30 +0100

    Initial Commit

---
+++ Validator/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Validator.Models;
+
+namespace Validator.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public HttpResponseMessage Post(Customer customer)
+        {
+            // Send an error response if the ModelState is invalid
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
---
+++ Validator/Validators/NonNegativeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Validator.Validators
+{
+    public class NonNegativeAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (!(value is int))
+            {
+                return new ValidationResult("NonNegativeAttribute can only be applied to ints.");
+            }
+            if ((int)value < 0)
+            {
+                return new ValidationResult("Property should be non-negative.");
+            }
+            return null;
+        }
+    }
+}
